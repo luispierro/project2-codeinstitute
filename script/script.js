f233b91eeed98e7017556b3c7783d927f97f4018
Luis Pierro <luisf.pierro@gmail.com>
2024-10-16 12:16:47 +0200

    Update js
    Add the variables and math random to choose a play for the machine

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
 function playGame() {
+    // Possible options for the machine to choose
+    let gameOption = [ 'rock', 'paper', 'scissor', 'lizard', 'spock']
     // Select the checked radio button with name="option"
     let selectedOption = document.querySelector('input[name="choice"]:checked');
     
     if (selectedOption) {
-      alert("Selected option: " + selectedOption.value);
-      
+        //get a number between 0 and 4
+        let randomNumber = Math.floor(Math.random() * 5);
+        alert("Selected option: " + selectedOption.value + "and the computer selected: " + gameOption[randomNumber]);
+
     } else {
       alert("No option selected");
     }
